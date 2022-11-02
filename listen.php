1ee5520968cab0defa81cb0fb948fa55bc1a4016
XXuYYang <40904591+XXuYYang@users.noreply.github.com>
2022-11-02 17:02:11 +0800

    Add files via upload
    listen

---
+++ listen.php
@@ -0,0 +1,22 @@
+<?php
+use Workerman\Worker;
+require_once __DIR__ . '\workerman\Autoloader.php';
+
+// 创建一个Worker监听2345端口，使用http协议通讯
+$worker = new Worker("websocket://0.0.0.0:8080");
+
+// 启动4个进程对外提供服务
+$worker->count = 1;
+
+// 接收到浏览器发送的数据时回复hello world给浏览器
+$worker->onMessage = function($connection, $message)use($worker)
+{
+    // 向浏览器发送hello world
+    foreach($worker->connections as $connection){
+        $connection->send($message);
+    }
+};
+
+// 运行worker
+Worker::runAll();
+?>
