7e58c43c3c08d013cf16ddd0272e3371c43be427
XXuYYang <2653976360@qq.com>
2022-11-03 16:16:12 +0800

    添加权限控制

--- listen.php
+++ listen.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 use Workerman\Worker;
 require_once __DIR__ . '\workerman\Autoloader.php';
@@ -6,41 +5,27 @@ require_once __DIR__ . '\workerman\Autoloader.php';
 // 创建一个Worker监听2345端口，使用http协议通讯
 $worker = new Worker("websocket://0.0.0.0:8080");
 
-// 启动4个进程对外提供服务
+// 启动1个进程对外提供服务
 $worker->count = 1;
-
-// 接收到浏览器发送的数据时回复hello world给浏览器
+$chmod = array( 'root' => 0, '123'=> 0 );
+// 接收到浏览器发送的数据时回复
 $worker->onMessage = function($connection, $message)use($worker)
 {
-    // 向浏览器发送hello world
-    foreach($worker->connections as $connection){
-        $connection->send($message);
+    global $chmod;
+    $data = explode(" ", $message);
+    $owner = $data[1];
+    if ( strcmp($data[0], 'chmod') == 0 ) { 
+        if ( strcmp($chmod[$data[1]], 0) == 0 ) $chmod[$data[1]] = 1;
+        else $chmod[$data[1]] = 0;
+    }
+
+    if ( strcmp($chmod[$data[1]], 0) == 0 ){
+        foreach($worker->connections as $connection){
+            $connection->send($message);
+        }
     }
 };
 
 // 运行worker
 Worker::runAll();
-=======
-<?php
-use Workerman\Worker;
-require_once __DIR__ . '\workerman\Autoloader.php';
-
-// 创建一个Worker监听2345端口，使用http协议通讯
-$worker = new Worker("websocket://0.0.0.0:8080");
-
-// 启动4个进程对外提供服务
-$worker->count = 1;
-
-// 接收到浏览器发送的数据时回复hello world给浏览器
-$worker->onMessage = function($connection, $message)use($worker)
-{
-    // 向浏览器发送hello world
-    foreach($worker->connections as $connection){
-        $connection->send($message);
-    }
-};
-
-// 运行worker
-Worker::runAll();
->>>>>>> 1ee5520968cab0defa81cb0fb948fa55bc1a4016
 ?>
